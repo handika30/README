0717b52c8f06559150214419d06200aeb865b759
handika30 <handikapermana2030@gmail.com>
2025-03-08 23:03:12 +0700

    tanggal dan hari.js

--- tanggal dan hari.js
+++ tanggal dan hari.js
@@ -1,1 +1,7 @@
+const date1 = Date.UTC(2024, 2, 19);  
+const date2 = Date.UTC(2024, 2, 21);
+
+const dayDifference = Math.abs((date2 - date1) / 86400000); // 86400000 = 1000 * 60 * 60 * 24
+
+console.log(`Output: ${dayDifference}`);
 
