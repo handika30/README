859a63282504b1dec91efa1649eda855cb5642a4
handika30 <handikapermana2030@gmail.com>
2025-03-08 21:57:22 +0700

    persegipanjang.js

--- persegipanjang.js
+++ persegipanjang.js
@@ -1,8 +1,3 @@
-function calculateRectangleArea(length, width) {
-    return length * width;
-}
-
-// Example usage
 let length = 5;
 let width = 3;
 let area = calculateRectangleArea(length, width);
