9b7b7a3397bff905710fffc37208106383ed6909
handika30 <handikapermana2030@gmail.com>
2025-03-08 23:07:29 +0700

    inisial name.js

--- inisial name.js
+++ inisial name.js
@@ -1,1 +1,10 @@
+function getInitials(name) {
+    return name
+        .split(" ") // Memisahkan nama berdasarkan spasi
+        .map(word => word.charAt(0).toUpperCase()) // Mengambil huruf pertama dan ubah ke kapital
+        .join(""); // Menggabungkan hasilnya
+}
+
+// Contoh penggunaan
+console.log(getInitials("Handika sti")); // Output: HS
 
