c486bcbdcc47d550654dd9570f7d81bcf7e7751a
handika30 <handikapermana2030@gmail.com>
2025-03-08 22:09:59 +0700

    luaslingkaran.js

--- luaslingkaran.js
+++ luaslingkaran.js
@@ -1,1 +1,8 @@
+const radius = 5;
+
+const diameter = radius * 2; // hasil: 10
+let circumference = 2 * (22 / 7) * radius; // hasil: 31.428571428571427
+let area = (22 / 7) * radius * radius; // hasil: 78.57142857142857
+
+console.log(`diameter = ${diameter}, circumference = ${circumference}, area = ${area}`);
 
